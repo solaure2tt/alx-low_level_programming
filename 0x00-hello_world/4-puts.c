f5368bb7bd478377d92fba6bcb524883217d56b5
Laure Tchuente <solaure2t@yahoo.fr>
2022-12-08 08:53:38 -0800

    update task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
-/* This program print a text by using the function puts */
+/* This program print a text by using the function puts 
+ * main - print programming is like building a multilingual puzzle
+ * Description: use puts to print in the output
+ * Return: 0
+ */
+
 int main(void)
 {
-	char ch[]="Programming is like building a multilingual puzzle";
+	char ch[] = "\"Programming is like building a multilingual puzzle";
+
 	puts(ch);
-	return 0;
+	return (0);
 }
