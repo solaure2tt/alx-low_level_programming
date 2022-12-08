cf962caf897d598811fe73dea0ce097ab4c585c2
Laure Tchuente <solaure2t@yahoo.fr>
2022-12-08 10:46:32 -0800

    task 7

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * main - print on the screen
+ * Description: print a text without use printf or puts
+ * Return: 1
+ */
+
+int main(void)
+{
+	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	int i,s;
+
+	s = strlen(str);
+	for(i=0;i<s;i++)
+	{
+		putchar(str[i]);
+	}
+	putchar('\n');
+	return (1);
+}
