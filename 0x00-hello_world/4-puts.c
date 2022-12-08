4cbf84823dbd6c1ada28f6b97d9df40cbcec3dd0
Laure Tchuente <solaure2t@yahoo.fr>
2022-12-08 08:58:49 -0800

    update task 4 betty style

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-/* This program print a text by using the function puts 
+/** 
  * main - print programming is like building a multilingual puzzle
  * Description: use puts to print in the output
  * Return: 0
