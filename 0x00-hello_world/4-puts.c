faef5696aa3f554f61c0ca90050658b2da975e26
Laure Tchuente <solaure2t@yahoo.fr>
2022-12-08 08:40:03 -0800

    task 4 and 5

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+/* This program print a text by using the function puts */
+int main(void)
+{
+	char ch[]="Programming is like building a multilingual puzzle";
+	puts(ch);
+	return 0;
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+/* this program print a text on screen by using the function printf */
+
+int main()
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return 0;
+}
