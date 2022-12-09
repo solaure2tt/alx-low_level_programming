3179f39631e37ef4d2e2ceff04f0da9debaeaaed
Laure Tchuente <solaure2t@yahoo.fr>
2022-12-09 13:18:56 -0800

    task 12

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - prints all possible different combinations of two two-digits
+ * Description: The two digits must be different and Numbers must be separated
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i, j, k, l;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = i; j < 10; j++)
+		{
+			while (j < i)
+				j++;
+			for (k = 0; k < 10; k++)
+			{
+				for (l = k+1; l < 10; l++)
+				{
+					putchar('0' + i);
+					putchar('0' + j);
+					putchar(' ');
+					putchar('0' + k);
+					putchar('0' + l);
+					if ((i == 9) && (j == 9) && (k == 8) && (l == 9))
+						break;
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
