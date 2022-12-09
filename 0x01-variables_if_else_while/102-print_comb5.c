1e7eaac480d65d71cd85ebafb3ef865656940614
Laure Tchuente <solaure2t@yahoo.fr>
2022-12-09 13:59:19 -0800

    task 12 update 4

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -23,7 +23,7 @@ int main(void)
 					if ((i == 0) && (j == 0) && (k == 0) && (l == 0))
 						l++;
 					while ((i == k) && (l <= j))
-						k++;
+						l++;
 					putchar('0' + i);
 					putchar('0' + j);
 					putchar(' ');
