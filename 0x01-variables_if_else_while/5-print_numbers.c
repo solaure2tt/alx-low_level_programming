66720dcc0e8a46ac559407a8989859de372393e2
Laure Tchuente <solaure2t@yahoo.fr>
2022-12-09 08:15:38 -0800

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - number
+ * Description: print all single digit number
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	while (i < 10)
+	{
+		printf("%d", i);
+		i++;
+	}
+	printf("\n");
+	return (0);
+}
