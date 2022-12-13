c09d0782d4718f7e96a57ffa7370081adf992512
Laure Tchuente <solaure2t@yahoo.fr>
2022-12-13 09:11:20 -0800

    task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print 10 time the alphabet
+ * Description: prints 10 times the alphabet, in lowercase
+ * Return: 0
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		print_alphabet();
+	return (0);
+}
