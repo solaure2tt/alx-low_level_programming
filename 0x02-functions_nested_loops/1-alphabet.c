23ee1143b8c36abbcf01e89a9ec564e9f6692d69
Laure Tchuente <solaure2t@yahoo.fr>
2022-12-13 07:35:35 -0800

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet - writes alphabet in stdout
+ * Description: prints the alphabet, in lowercase
+ * Return: 0
+ */
+void print_alphabet(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+	}
+	 _putchar('\n');
+
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,5 +2,6 @@
 #define _PUTCHAR_FILE
 
 int _putchar(char c);
+void print_alphabet(void);
 
 #endif
