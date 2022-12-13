0d04c86dff525f996817c6a50f79d0d937cd96a4
Laure Tchuente <solaure2t@yahoo.fr>
2022-12-13 10:57:28 -0800

    main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -5,5 +5,10 @@ int _putchar(char c);
 void print_alphabet(void);
 int _islower(int c);
 void print_alphabet_x10(void);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int n);
+int print_last_digit(int);
+int add(int a, int b);
 
 #endif
