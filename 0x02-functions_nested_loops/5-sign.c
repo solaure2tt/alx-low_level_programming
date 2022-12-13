fd0795fcd10b272eba5c45a6c83d4ca98fe187c9
Laure Tchuente <solaure2t@yahoo.fr>
2022-12-13 11:10:17 -0800

    task 5 update 1

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -11,10 +11,19 @@ int print_sign(int n)
 	int result;
 
 	if (n > 0)
+	{
+		_putchar('+');
 		result = 1;
+	}
 	else if ( n == 0)
+	{
 		result = 0;
+		_putchar('0');
+	}
 	else
+	{
+		_putchar('-');
 		result = -1;
+	}
 	return (result);
 }
