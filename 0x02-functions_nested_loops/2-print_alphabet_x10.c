2fa21594b023c707abcf1b501cd456195de031ed
Laure Tchuente <solaure2t@yahoo.fr>
2022-12-13 09:22:20 -0800

    task 2 update 4

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,7 +8,15 @@
 void print_alphabet_x10(void)
 {
 	int i;
+	char c = 'a';
 
 	for (i = 0; i < 10; i++)
-		print_alphabet();
+	{
+		while (c <= 'z')
+		{
+			_putchar(c);
+			c++;
+		}
+		_putchar('\n');
+	}
 }
