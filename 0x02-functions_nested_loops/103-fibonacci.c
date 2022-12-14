95eb4d3ac6e47a1b1c53710638eebbb670f2468c
Laure Tchuente <solaure2t@yahoo.fr>
2022-12-13 17:27:57 -0800

    task 15

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - entry
+ * Description: program that finds and prints the sum of the even-valued terms
+ * Return: 0 success
+ */
+int main(void)
+{
+	long int i = 1, j =2;
+	long int n = i + j;
+	long int res = 0;
+
+	while (n < 4000000)
+	{
+		if ((n % 2) == 0)
+			res = res + n;
+		i = j;
+		j = n;
+		n = i + j;
+	}
+	printf("%lu\n", res);
+	return (0);
+}
