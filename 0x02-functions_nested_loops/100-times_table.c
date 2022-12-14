b63b321eef8879a2401fe9ac133dd0a05bb2a02d
Laure Tchuente <solaure2t@yahoo.fr>
2022-12-13 20:58:05 -0800

    task 12 upd 5

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -6,17 +6,10 @@
  * @n: number in entry
  * Return: numbers
  */
-void print_times_table(int n)
+void printR(int res, int i)
 {
-	int i, x, res, first, last, middle;
-
-	if ((n >= 0) && (n <= 15))
-	{		
-	for (x = 0; x <= n; x++)
-	{	
-		for (i = 0; i <= n; i++)
-		{
-				res = i * x;
+	int first, last, middle;
+	
 				if (res > 9)
 				{
 					_putchar(' ');
@@ -46,12 +39,55 @@ void print_times_table(int n)
 					_putchar(' ');
 					_putchar('0' + res);
 				}
+
+}
+void print_times_table(int n)
+{
+	int i, x, res;
+
+	if ((n >= 0) && (n <= 15))
+	{		
+		for (x = 0; x <= n; x++)
+		{
+			for (i = 0; i <= n; i++)
+			{
+				res = i * x;
+				printR(res, i);
+				/*if (res > 9)
+				{
+					_putchar(' ');
+					first = res / 10;
+					last = res % 10;
+					if (res <= 99)
+					{
+						_putchar(' ');
+						_putchar('0' + first);
+						_putchar('0' + last);
+					}
+					else
+					{
+						middle = first % 10;
+						first = first / 10;
+						_putchar('0' + first);
+						_putchar('0' + middle);
+						_putchar('0' + last);
+					}
+				}
+				else if (i == 0)
+					_putchar('0' + res);
+				else
+				{
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar('0' + res);
+				}*/
 				if (i == n)
 					_putchar('\n');
 				else
 					_putchar(',');
 			
+			}
 		}
-	}
 }
 }
