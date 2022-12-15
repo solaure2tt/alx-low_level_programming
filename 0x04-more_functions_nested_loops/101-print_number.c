15148849a2bb9dbe6a23e261c0088528d1e970b1
Laure Tchuente <solaure2t@yahoo.fr>
2022-12-15 14:46:27 -0800

    task 12

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * print_number - entry
+ * Description: print an integer
+ * @n: parameter
+ * Result: number
+ */
+void print_number(int n)
+{
+	int rev;
+
+	rev = 0;
+	if (n < 0)
+		_putchar('-');
+	while (n / 10)
+	{
+		rev = rev * 10 + n % 10;
+		n = n/ 10;
+	}
+	if (rev < 0)
+	{
+		rev = rev * (-1);
+		n = n * (-1);
+	}
+	_putchar(n + '0');
+	while (rev)
+	{
+		_putchar(rev % 10 + '0');
+		rev = rev / 10;
+	}
+}
