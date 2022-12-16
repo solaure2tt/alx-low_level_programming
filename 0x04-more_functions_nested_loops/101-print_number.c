aa675c6a820b7337679fb7edf67f13f9e97bcc7f
Laure Tchuente <solaure2t@yahoo.fr>
2022-12-15 16:34:29 -0800

    task 11 up 33

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -8,13 +8,25 @@
  */
 void print_number(int n)
 {
+	int rev;
 
+	rev = 0;
 	if (n < 0)
-	{
 		_putchar('-');
+	while (n / 10)
+	{
+		rev = rev * 10 + n % 10;
+		n = n / 10;
+	}
+	if (rev < 0)
+	{
+		rev = -rev;
 		n = -n;
 	}
-	if (n / 10)
-		print_number(n / 10);
-	_putchar(n % 10 + '0');
+	_putchar(n + '0');
+	while (rev)
+	{
+		_putchar(rev % 10 + '0');
+		rev = rev / 10;
+	}
 }
