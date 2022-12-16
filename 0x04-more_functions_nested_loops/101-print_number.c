213f134b4c52ec32ba7f2ffc5b59e5aac1cfdcb2
Laure Tchuente <solaure2t@yahoo.fr>
2022-12-15 16:45:37 -0800

    task 11 updf

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -8,25 +8,13 @@
  */
 void print_number(int n)
 {
-	int rev;
 
-	rev = 0;
 	if (n < 0)
+	{
 		_putchar('-');
-	while (n / 10)
-	{
-		rev = rev * 10 + n % 10;
-		n = n / 10;
-	}
-	if (rev < 0)
-	{
-		rev = -rev;
 		n = -n;
 	}
-	_putchar(n + '0');
-	while (rev)
-	{
-		_putchar(rev % 10 + '0');
-		rev = rev / 10;
-	}
+	if (n / 10)
+		print_number(n / 10);
+	_putchar(n % 10 + '0');
 }
