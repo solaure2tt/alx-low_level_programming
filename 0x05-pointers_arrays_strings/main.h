6c70a963e98eeaaa20c662ae394cacaadf42a916
Laure Tchuente <solaure2t@yahoo.fr>
2022-12-19 04:33:34 -0800

    main.h

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_POINT
+#define MAIN_POINT
+
+void reset_to_98(int *n);
+int _putchar(char c);
+void swap_int(int *a, int *b);
+int _strlen(char *s);
+void _puts(char *str);
+void print_rev(char *s);
+
+#endif
