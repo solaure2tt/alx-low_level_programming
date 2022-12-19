3ebcddaae81bdee90309f5110d7886cad46b20da
Laure Tchuente <solaure2t@yahoo.fr>
2022-12-19 11:58:25 -0800

    task 11

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * randomPassword - random passwor generate
+ * Description: generate a randon password
+ * @n: parameter 1
+ * Return: nothing
+ */
+void randomPassword(int n)
+{
+	int i = 0;
+	int test = 0;
+	char letters[] = "abcdefghijklmnpqrstuvwxyz";
+	char lettersUpper[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char numbers[] = "0123456789";
+	char symb[] = "!@#$^&*?";
+	char pwd[n];
+
+	srand((unsigned int)(time(NULL)));
+	test = rand() % 4;
+	for (i = 0; i < n; i++)
+	{
+		if (test == 1)
+		{
+			pwd[i] = numbers[rand() % 10];
+			test = rand() % 4;
+			printf("%c", pwd[i]);
+		}
+		else if (test == 2)
+		{
+			pwd[i] = symb[rand() % 8];
+			test = rand() % 4;
+			printf("%c", pwd[i]);
+		}
+		else if (test == 3)
+		{
+			pwd[i] = lettersUpper[rand() % 26];
+			test = rand() % 4;
+			printf("%c", pwd[i]);
+		}
+		else
+		{
+			pwd[i] = letters[rand() % 26];
+			test = rand() % 4;
+			printf("%c", pwd[i]);
+		}
+	}
+}
+
+/**
+ * main - entry
+ * Description: random password
+ * Return: 0 success
+ */
+int main()
+{
+	int n = 10;
+
+	randomPassword(n);
+	return (0);
+}
