a54062a152678f0585ab2b5661cfe8905c3b55f0
Laure Tchuente <solaure2t@yahoo.fr>
2022-12-19 09:12:16 -0800

    task 8 upd3

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -31,7 +31,7 @@ void print_array(int *a, int n)
 {
 	int i;
 
-	if (n >= 0)
+	if (n > 0)
 	{
 		for (i = 0; i < n; i++)
 		{
@@ -43,4 +43,5 @@ void print_array(int *a, int n)
 	}
 	else
 		printf("\n");
+
 }
