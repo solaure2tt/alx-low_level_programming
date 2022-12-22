d24ee0b1eb5c0660c0a3cd1dd972232b8ed78cdd
Laure Tchuente <solaure2t@yahoo.fr>
2022-12-21 18:43:24 -0800

    main.h

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -9,5 +9,8 @@ int _strcmp(char *s1, char *s2);
 void reverse_array(int *a, int n);
 char *string_toupper(char *str);
 char *cap_string(char *s);
+char *leet(char *s);
+char *rot13(char *s);
+void print_number(int n);
 
 #endif
