4d079e1d3d113c584b18dc29cda98726e97f1b86
Laure Tchuente <solaure2t@yahoo.fr>
2023-01-02 18:51:13 -0800

    update 3 task 7

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -10,35 +10,17 @@
 void print_chessboard(char (*a)[8])
 {
 	int i, j = 0;
-	int hascontent = 0;
 
 	i = 0;
-	while (a[i][j] != '\0')
+	while ((a[i] != NULL) && (a[i][j] != '\0'))
 	{
-		hascontent = 0;
 		j = 0;
 		while (j < 8)
 		{
-			if (a[i][j] >= 'a' && a[i][j] <= 'z')
-			{
-				_putchar(a[i][j]);
-				hascontent = 1;
-			}
-			if (a[i][j] >= 'A' && a[i][j] <= 'Z')
-			{
-				_putchar(a[i][j]);
-				hascontent = 1;
-			}
-			if (a[i][j] == ' ')
-			{
-				_putchar(' ');
-				hascontent = 1;
-				break;
-			}
+			_putchar(a[i][j]);
 			j++;
 		}
 		i++;
-		if (hascontent == 1)
-			_putchar('\n');
+		_putchar('\n');
 	}
 }
