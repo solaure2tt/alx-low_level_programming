60546b6978c5f43dad702b04c1b40430e1d9745c
Laure Tchuente <solaure2t@yahoo.fr>
2023-01-04 15:21:21 -0800

    task 7

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+ * length - length of string
+ * Description: calculate the lenth of string
+ * @s: parameter 1
+ * Return: number
+ */
+int length(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	s++;
+	return (length(s) + 1);
+}
+/**
+ * palindrome -  palindrome
+ * Description: palindrome string
+ * @s: parameter 1
+ * @init: parameter 2
+ * @size: parameter 3
+ * Return: nothing
+ */
+int palindrome(char *s, int init, int size)
+{
+	if (size <= init)
+		return (1);
+	if (s[init] == s[size])
+		return (palindrome(s, init + 1, size - 1));
+	return (0);
+}
+/**
+ * is_palindrome - palindrome with recursion
+ * Description: function that returns 1 if a string is a palindrome and 0 if n
+ * @s: parameter
+ * Return: 1 or 0
+ */
+int is_palindrome(char *s)
+{
+	int size = length(s);
+
+	if(size == 0)
+		return (0);
+	return (palindrome(s, 0, size - 1));
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -8,5 +8,9 @@ int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int is_prime_number(int n);
-
+int is_palindrome(char *s);
+int wildcmp(char *s1, char *s2);
+int cmp (char *s1, char *s2, int i, int j);
+int cmp1(char *s1, char *s2, int i, int j);
+int cmp2(char *s1, char *s2, int i, int j);
 #endif
