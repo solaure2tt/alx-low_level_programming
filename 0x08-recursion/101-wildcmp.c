02522a77c8640230c53e6e073440ecb3390f4cc8
Laure Tchuente <solaure2t@yahoo.fr>
2023-01-05 06:43:52 -0800

    update 5 task 8

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -18,7 +18,7 @@ int cmp1(char *s1, char *s2, int i, int j)
 		return (1);
 	if (s1[i + 1] == '*')
 	{
-		if (s1[i + 1] == '\0')
+		if (s1[i + 2] == '\0')
 			return (1);
 		return (cmp1(s1, s2, i + 1, j));
 	}
@@ -45,7 +45,7 @@ int cmp2(char *s1, char *s2, int i, int j)
 		return (1);
 	if (s2[j + 1] == '*')
 	{
-		if (s2[j + 1] == '\0')
+		if (s2[j + 2] == '\0')
 			return (1);
 		return (cmp2(s1, s2, i, j + 1));
 	}
@@ -72,12 +72,20 @@ int cmp(char *s1, char *s2, int i, int j)
 	{
 		if (s1[i + 1] == '\0')
 			return (1);
+		if (s1[i + 1] == s2[j])
+			return (cmp(s1, s2, i + 1, j));
+		if (s2[j + 1] == '\0')
+			return (cmp(s1, s2, i + 1, j));
 		return (cmp1(s1, s2, i, j + 1));
 	}
 	if (s2[j] == '*')
 	{
 		if (s2[j + 1] == '\0')
 			return (1);
+		if (s2[j + 1] == s1[i])
+			return (cmp(s1, s2, i, j + 1));
+		if (s1[i + 1] == '\0')
+			return (cmp(s1, s2, i, j + 1));
 		return (cmp2(s1, s2, i + 1, j));
 	}
 	return (0);
