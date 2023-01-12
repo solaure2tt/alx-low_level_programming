dc02d04e7e1c06c668206afb1fe08994d98192c0
Laure Tchuente <solaure2t@yahoo.fr>
2023-01-12 11:44:38 -0800

    task 1

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,63 @@
+#include "main.h"
+#include <stdlib.h>
+#include <unistd.h>
+
+/**
+ * _strlen - size of a string
+ * Description: function that returns the length of a string
+ * @s: parameter
+ * Return: number
+ */
+
+int _strlen(char *s)
+{
+	int i, res = 0;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		res++;
+		i++;
+	}
+	return (res);
+}
+/**
+ * string_nconcat - entry
+ * Description: function that concatenates two strings.
+ * @s1: parameter 1
+ * @s2: parameter 2
+ * @n: parameter 3
+ * Return: pointer
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *res, *tmp;
+	unsigned int i = 0;
+	int size = _strlen(s1) + 1 + n;
+
+	if (s1 ==  NULL)
+		s1 = "\0";
+	if (s2 == NULL)
+		s2 = '\0';
+	if (n == 0)
+		return (s1);
+	res = malloc(sizeof(char) * size);
+	if (res == NULL)
+		return (NULL);
+	tmp = res;
+	while (*s1 != '\0')
+	{
+		*res = *s1;
+		res++;
+		s1++;
+	}
+	while ((i < n) && (*s2 != '\0'))
+	{
+		*res = *s2;
+		res++;
+		s2++;
+		i++;
+	}
+	*res = '\0';
+	return (tmp);
+}
