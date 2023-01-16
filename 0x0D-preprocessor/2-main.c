f569125772470c795e2fddd9905b6ede524df8dd
Laure Tchuente <solaure2t@yahoo.fr>
2023-01-16 10:50:16 -0800

    task 2 update

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,13 +1,30 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <unistd.h>
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ * Return: On success 1.On error, -1 is returned, and errno is set appropriatel
+ */
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
 
 /**
  * main - entry
  * Description: program that prints the name of the file it was compiled from
  * Return: 0 success 1 no success
  */
-int main()
+int main(void)
 {
-	printf("%s\n", __FILE__);
+	char *file = __FILE__;
+
+	while (*file != '\0')
+	{
+		_putchar(*(file));
+		file++;
+	}
+	_putchar('\n');
 	return (0);
 }
