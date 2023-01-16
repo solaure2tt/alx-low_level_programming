5f36823f982ff9dae5c2d53a350e9c0c4e160bef
Laure Tchuente <solaure2t@yahoo.fr>
2023-01-16 09:27:42 -0800

    update task 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -12,10 +12,35 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	struct dog *d;
+	char *n;
+	char *o;
+	char *na = name;
+	char *ow = owner;
+	int s;
 
+	s = 0;
 	d = malloc(sizeof(struct dog));
 	if (d == NULL)
 		return (NULL);
+	while (*na != '0')
+	{
+		s++;
+		na++;
+	}
+	n = malloc(s * sizeof(char));
+	if (n == NULL)
+		return (NULL);
+	n = name;
+	s = 0;
+	while (*ow != '\0')
+	{
+		s++;
+		ow++;
+	}
+	o = malloc(s * sizeof(char));
+	if (o == NULL)
+		return (NULL);
+	o = owner;
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
