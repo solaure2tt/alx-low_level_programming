067e735cf3f239e8bba05daf292c8fc7da7c3b83
Laure Tchuente <solaure2t@yahoo.fr>
2023-01-17 07:35:40 -0800

    task upd3

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -21,7 +21,7 @@ void print_dog(struct dog *d)
 			printf("Name: %s\n", d->name);
 		}
 		printf("Age: %f\n", d->age);
-		if ((d->owner == NULL) || (d->owner == ""))
+		if (d->owner == NULL)
 		{
 			printf("OwnerN: (nil)\n");
 		}
