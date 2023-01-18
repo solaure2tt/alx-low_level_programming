5a67fabe83b60f68ae3dbdde1bf8d426ac47cad9
Laure Tchuente <solaure2t@yahoo.fr>
2023-01-18 08:38:36 -0800

    task 1 update 1

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -14,10 +14,16 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t i;
 
-	i = 0;
-	while (i < size)
+	if (action != NULL)
 	{
-		action(array[i]);
-		i++;
+		i = 0;
+		if (size > 0)
+		{
+			while (i < size)
+			{
+				action(array[i]);
+				i++;
+			}
+		}
 	}
 }
