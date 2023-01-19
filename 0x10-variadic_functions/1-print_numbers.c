e7ef44f3f9dc525e23b7a6184b6c3f18cc401266
Laure Tchuente <solaure2t@yahoo.fr>
2023-01-19 07:05:20 -0800

    task 1

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,31 @@
+#include "variadic_functions.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdarg.h>
+#include <stddef.h>
+/**
+ * print_numbers - prints parameters
+ * Description:  function that prints numbers, followed by a new line
+ * @separator: parameter 1
+ * @n: parameter 2
+ * Return: nothing
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list args;
+	unsigned int i;
+
+	if (n != 0)
+	{
+		va_start(args, n);
+		for (i = 0; i < n; i++)
+		{
+			printf("%d", va_arg(args, int));
+			if (((i + 1) < n) && (separator != NULL))
+				printf("%s", separator);
+
+		}
+		va_end(args);
+		printf("\n");
+	}
+}
