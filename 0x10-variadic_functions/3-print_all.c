9ec380a3cffdbea98264cde34ba19b3f66291a2c
Laure Tchuente <solaure2t@yahoo.fr>
2023-01-24 16:20:59 -0800

    update eror 1 task 3

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -36,10 +36,7 @@ void print_all(const char * const format, ...)
 					break;
 				case 's':
 					s = va_arg(args, char*);
-					if (s == NULL)
-						printf("(nil)");
-					else
-						printf("%s", s);
+					printf("%s", s);
 					break;
 				default:
 					ignore = 1;
