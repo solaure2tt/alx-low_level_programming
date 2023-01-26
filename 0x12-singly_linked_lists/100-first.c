0750527d1cd19eb8f01906cfd584b9b1c32d8b0f
Laure Tchuente <solaure2t@yahoo.fr>
2023-01-26 09:36:53 -0800

    task 100

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * print - print on screen
+ * Description: print a text on screen
+ * Return: nothing
+ */
+void __attribute__ ((constructor)) print()
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
