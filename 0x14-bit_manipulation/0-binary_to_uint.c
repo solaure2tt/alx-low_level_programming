88d3c1a37fc77b8b88e0e5b3006f5b2592a1fd9a
Laure Tchuente <solaure2t@yahoo.fr>
2023-02-02 17:40:17 -0800

    task 0 update 3

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -3,52 +3,23 @@
 #include <stdlib.h>
 
 /**
- * _pow - pow of number
- * Description: calculate the power of a number
- * @x: number
- * @y: pow
- * Return: number
- */
-unsigned int _pow(unsigned int x, unsigned int y)
-{
-	unsigned int res = 1;
-
-	while (y > 0)
-	{
-		res = res * x;
-		y--;
-	}
-	return (res);
-}
-/**
  * binary_to_uint - binary to number
  * Description: function that converts a binary number to an unsigned int
  * @b: number to convert
- * Return: number
+ * Return: decimal number
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int n, res = 0;
-	const char *tmp = b;
+	unsigned int i, res = 0;
 
-	n = 0;
+	i = 0;
 	if (b == NULL)
 		return (0);
-	while (*tmp != '\0')
+	while (b[i] == '0' || b[i] == '1')
 	{
-		if (*tmp != '0' && *tmp != '1')
-			return (0);
-		n++;
-		tmp++;
-	}
-	tmp = b;
-	n--;
-	while (*tmp != '\0')
-	{
-		res = res + ((*tmp - '0') * _pow(2, n));
-		n--;
-		tmp++;
-
+		res <<= 1;
+		res += b[i] - '0';
+		i++;
 	}
 	return (res);
 }
