d061ee1d329dd54e2c646841d9202e93be6663f1
Laure Tchuente <solaure2t@yahoo.fr>
2023-02-07 07:10:44 -0800

    update permissions task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,13 +18,15 @@ int create_file(const char *filename, char *text_content)
 {
 	ssize_t __attribute__((unused)) resw;
 	int fd;
+	mode_t mode;
 
 	if (filename == NULL)
 		return (-1);
 	fd = open(filename, O_WRONLY);
 	if (fd < 0)
 	{
-		fd = open(filename, O_WRONLY | O_CREAT, S_IRUSR, S_IWUSR);
+		mode = S_IRUSR | S_IWUSR;
+		fd = open(filename, O_WRONLY | O_CREAT, mode);
 	}
 	else
 	{
