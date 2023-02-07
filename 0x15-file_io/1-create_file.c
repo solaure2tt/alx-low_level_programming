138c59cb5b6e18fdb5c1b1bb856b70e53d0c7a08
Laure Tchuente <solaure2t@yahoo.fr>
2023-02-07 07:00:02 -0800

    update task 1 second

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -21,8 +21,16 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	fd = open(filename, O_RDWR | O_CREAT, S_IRUSR, S_IWUSR, O_TRUNC);
+	fd = open(filename, O_WRONLY);
 	if (fd < 0)
+	{
+		fd = open(filename, O_WRONLY | S_IRUSR, S_IWUSR);
+	}
+	else
+	{
+		fd = open(filename, O_WRONLY | O_TRUNC);
+	}
+	if (fd == -1)
 		return (-1);
 	if (text_content != NULL)
 	{
