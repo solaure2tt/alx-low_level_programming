6ede26beb5e8b3ea10fb8e89e20d1a41515c4141
Laure Tchuente <solaure2t@yahoo.fr>
2023-02-07 07:17:08 -0800

    update stderr task 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -21,37 +21,37 @@ int main(int ac, char **av)
 
 	if (ac != 3)
 	{
-		dprintf(STDOUT_FILENO, "Usage: cp file_from file_to\n");
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		return (97);
 	}
 	fd1 = open(av[1], O_RDONLY);
 	if (fd1 == -1)
 	{
-		dprintf(STDOUT_FILENO, "Error: Can't read from file %s\n", av[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
 		return (98);
 	}
 	fd2 = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (fd2 == -1)
 	{
-		dprintf(STDOUT_FILENO, "Error: Can't write to %s\n", av[2]);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
 		return (99);
 	}
 	while ((resr = read(fd1, buf, 1024)) > 0)
 	{
 		if ((write(fd2, buf, resr)) == -1)
 		{
-			dprintf(STDOUT_FILENO, "Error: Can't write to %s\n", av[2]);
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
 			return (99);
 		}
 	}
 	if (resr == -1)
 	{
-		dprintf(STDOUT_FILENO, "Error: Can't read from file %s\n", av[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
 		return (97);
 	}
 	if (close(fd1) == -1 || close(fd2) == -1)
 	{
-		dprintf(STDOUT_FILENO, "Error: Can't close fd FD_VALUE\n");
+		dprintf(STDERR_FILENO, "Error: Can't close fd FD_VALUE\n");
 		return (100);
 	}
 	return (0);
