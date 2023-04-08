3372b59e70d0873b4e7185244f60930202848c45
Laure Tchuente <solaure2t@yahoo.fr>
2023-04-07 17:46:05 -0700

    task 1 update betty style

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -14,11 +14,10 @@ unsigned long int hash_djb2(const unsigned char *str)
 {
 	unsigned long int hash;
 	int c;
-	
-	hash = 5381;
+
 	while ((c = *str++))
 	{
-		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+		hash = ((hash << 5) + hash) + c;
 	}
 	return (hash);
 }
